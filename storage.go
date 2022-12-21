a714779f1a69d024354f96398bda0699ff7e4539
Daniel Enman <enmand@gmail.com>
2022-12-21 08:38:57 -0400

    chore: add Type to CAS interface for human-readable name

--- storage.go
+++ storage.go
@@ -4,6 +4,13 @@ import (
 	"io"
 )
 
+// CASType is the type of content addressable storage.
+type CASType string
+
+func (c CASType) String() string {
+	return string(c)
+}
+
 type CAS interface {
 	io.Closer
 	Start() error
@@ -11,4 +18,6 @@ type CAS interface {
 	Get(id string) ([]byte, error)
 	// Will automatically zip to gzip
 	Put(data []byte) (string, error)
+	// Type returns the type of the CAS
+	Type() CASType
 }
